027575e52485e19ce66b3c28e0be634d9a74a611
VECTORPRO <Vector59rus@gmail.com>
2022-01-31 19:50:04 +0300

    Добавьте файлы проекта.

---
+++ SB19.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+template<class T>
+class Animal
+{
+	T value;
+public:
+	Animal() {}
+	Animal(T _value) : value(_value) {}
+
+	virtual void Voice()
+	{
+		value = "Ra-ta-ta";
+		std::cout << value << "\n";
+	}
+
+};
+
+class Dog:Animal<std::string>
+{
+	std::string value;
+public:
+	void Voice() override
+	{
+		value = "Woof";
+		std::cout << value << "\n";
+	}
+
+};
+
+
+
+int main()
+{
+	Animal<std::string>* ptrInt = new Animal<std::string>();
+	ptrInt->Voice();
+
+}
